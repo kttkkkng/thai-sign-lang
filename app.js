68cd5de2db7be96005ac4f37c1ef9b35f09ebbb5
kttkkkng <teerawat7971@gmail.com>
2022-07-28 14:31:45 +0700

    routing

--- app.js
+++ app.js
@@ -2,15 +2,33 @@ const app = Vue.createApp({
     data () {
         return {
             username: "",
-            password: ""
+            password: "",
+            confirm_password: "",
         }
     },
     methods: {
         goToLogin () {
+            console.log("test");
             window.location = "/login.html";
         },
         goToRegister () {
             window.location = "/register.html";
+        },
+        login () {
+            if (this.username == "" || this.password == "") {
+                alert("please enter username and password");
+            } else {
+                // TODO axios
+            }
+        },
+        register () {
+            if (this.username == "" || this.password == "" || this.confirm_password == "") {
+                alert("please enter username and password");
+            } else if (this.password != this.confirm_password) {
+                alert("please enter same password");
+            } else {
+                // TODO axios
+            }
         }
     }
 });
